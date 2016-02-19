cad08260edded75dced75ede547c1a21854d0f8d
baqir00 <baqirkhan00@gmail.com>
2016-02-19 21:00:53 +0530

    Home page completed

--- config/config.php
+++ config/config.php
@@ -23,9 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://'.$_SERVER['SERVER_NAME'].'/booksnstud';
-$config['index_page'] = '';
-$config['uri_protocol'] = 'AUTO';
+$config['base_url'] = 'http://localhost/booksnstud/';
 
 /*
 |--------------------------------------------------------------------------
